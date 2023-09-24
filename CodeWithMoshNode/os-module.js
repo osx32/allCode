811adb16872303c107033c49b88448198d250113
Kaan Çoban <kaaan3452@gmail.com>
2023-09-24 14:14:31 +0300

    Code With Mosh NodeJS
    Code With Mosh NodeJS all file wrote by myself

---
+++ CodeWithMoshNode/os-module.js
@@ -0,0 +1,9 @@
+const os = require('os');
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+console.log('Total Memory: ' + totalMemory);
+console.log('Free Memory: ' + freeMemory);
+
+//Template string
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
